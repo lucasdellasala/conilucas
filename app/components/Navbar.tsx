a56c03fd419f31decf3f82a68ed4623a496e1c3a
lucasdellasala <lucasdellasala@gmail.com>
2024-09-13 16:07:59 -0300

    fix: gift navbar link

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -8,13 +8,13 @@ export function Navbar() {
     <nav className={`fixed bottom-0 md:top-0 md:bottom-auto left-0 w-full bg-[rgba(0,0,0,0.3)] p-4 z-10 text-sm sm:text-base md:text-xl lg:text-3xl ${focusFont.className}`}>
       <ul className="grid grid-cols-5 gap-2 place-items-center">
         <li className="flex justify-center align-middle items-center">
-          <Link href="#info" className="hover:text-gray-400">
-            Info
+          <Link href="#location" className="hover:text-gray-400">
+          Ubicación
           </Link>
         </li>
         <li className="flex justify-center align-middle items-center">
-          <Link href="#location" className="hover:text-gray-400">
-            Ubicación
+          <Link href="#dresscode" className="hover:text-gray-400">
+          Dresscode
           </Link>
         </li>
         <li className="flex justify-center align-middle items-center">
@@ -23,13 +23,13 @@ export function Navbar() {
           </Link>
         </li>
         <li className="flex justify-center align-middle items-center">
-          <Link href="#dresscode" className="hover:text-gray-400">
-            Dresscode
+          <Link href="#response" className="hover:text-gray-400">
+          Responder
           </Link>
         </li>
         <li className="flex justify-center align-middle items-center">
-          <Link href="#response" className="hover:text-gray-400">
-            Responder
+          <Link href="#gift" className="hover:text-gray-400">
+            Regalo
           </Link>
         </li>
       </ul>
