8ac95e45e78435604a124039b4fd3b0a4a47bdae
Lucas Della Sala <lucasdellasalamarkovic@gmail.com>
2024-09-15 16:15:12 -0300

    Update Dresscode.tsx

--- app/sections/Dresscode.tsx
+++ app/sections/Dresscode.tsx
@@ -4,10 +4,10 @@ import { backgroundColors } from "../config";
 export function Dresscode() {
   return (
     <Template backgroundColor={backgroundColors[5]}>
-      <Subtitle className="pb-8">Dresscode: Etiqueta</Subtitle>
+      <Subtitle className="pb-8">Dresscode: Etiqueta | Black Tie</Subtitle>
       <Paragraph className="!text-left pt-4 lg:pt-0">
         En este día tan importante nos gustaría que cada uno de ustedes brille
-        con elegancia y estilo.
+        con elegancia y estilo, con el dresscode de etiqueta, también conocido como Black Tie.
       </Paragraph>
     </Template>
   );
